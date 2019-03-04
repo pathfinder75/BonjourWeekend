1b842132dade6d0cff838a654ed86abde8d28d81
yvescarrere <carrere.yves@gmail.com>
2019-03-05 00:07:26 +0100

    first commit

---
+++ app/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace BonjourWeekend
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			WriteLine("What is your name...?");
+			String UserName = ReadLine();
+			String Jour = String.Format($"{DateTime.Now.DayOfWeek}");
+
+			int Heure = DateTime.Now.Hour;
+
+			if (Jour == "Monday" && Heure > 9 || Jour == "Tuesday" || Jour == "Wednedsay" || Jour == "Thursday" || Jour == "Friday" && Heure < 17)
+			{
+
+
+				WriteLine($"Bonjour {UserName} vous etes en semaine");
+
+
+			}
+			else if(Jour == "Saturday" || Jour == "Sunday" || Jour == "Monday" & Heure < 9 || Jour == "Friday" && Heure > 17)
+			{
+				WriteLine($"Bonjour {UserName} vous etes en Week-end");
+			}
+		}
+	}
+}
